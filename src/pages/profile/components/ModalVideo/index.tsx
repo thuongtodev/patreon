cd2e321a5b7ccc6df395a10637e6a17ba38faa62
Tô Thành Thương <thuongthanhto@gmail.com>
2020-07-13 21:30:22 +0700

    fix

--- src/pages/profile/components/ModalVideo/index.tsx
+++ src/pages/profile/components/ModalVideo/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactPlayer from 'react-player';
 import Modal from 'react-modal';
+import closeIcon from './closeIcon.svg';
 
 const customStyles = {
   content: {
@@ -24,13 +25,19 @@ interface Props {
 const ModalVideo: React.FC<Props> = ({ open, toggleModal }) => {
   return (
     <Modal isOpen={open} onRequestClose={toggleModal} style={customStyles}>
+      <div
+        onClick={() => toggleModal()}
+        style={{ textAlign: 'right', marginRight: '-15px' }}
+      >
+        <img src={closeIcon} alt="" />
+      </div>
       <ReactPlayer
         url="https://storage.googleapis.com/superpeer-prod.appspot.com/hosts/Wrg3QfdEPDmrjE8jQkLU/video-1591320054285-transcoded.mp4?fbclid=IwAR16D4raG-L50EW-If5O53Lvh3ASeAb_FNb02FuQLixtGosNFPvl9VSlMB0"
         playing
         loop
         controls
         width="100%"
-        height="calc(100vh - 100px)"
+        height="100%"
       />
     </Modal>
   );
