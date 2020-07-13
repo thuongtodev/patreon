d2451f1256813ede219b06eff14a43dc7fafd782
Tô Thành Thương <thuongthanhto@gmail.com>
2020-07-14 06:57:10 +0700

    add play mute

--- src/pages/profile/components/ModalVideo/index.tsx
+++ src/pages/profile/components/ModalVideo/index.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
 import Modal from 'react-modal';
-import closeIcon from './closeIcon.svg';
+import { AiOutlineClose } from 'react-icons/ai';
+import { FaVolumeMute, FaVolumeUp } from 'react-icons/fa';
 
 const customStyles = {
   content: {
@@ -23,6 +24,8 @@ interface Props {
 }
 
 const ModalVideo: React.FC<Props> = ({ open, toggleModal }) => {
+  const [mute, setMute] = useState(false);
+
   return (
     <Modal isOpen={open} onRequestClose={toggleModal} style={customStyles}>
       <div style={{ position: 'relative' }}>
@@ -36,13 +39,31 @@ const ModalVideo: React.FC<Props> = ({ open, toggleModal }) => {
             zIndex: 9999999,
           }}
         >
-          <img src={closeIcon} alt="" />
+          <AiOutlineClose style={{ color: '#fff', width: 30, height: 30 }} />
+        </div>
+        <div
+          onClick={() => setMute(!mute)}
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            cursor: 'pointer',
+            zIndex: 9999999,
+          }}
+        >
+          {!mute && (
+            <FaVolumeUp style={{ color: '#fff', width: 30, height: 30 }} />
+          )}
+          {mute && (
+            <FaVolumeMute style={{ color: '#fff', width: 30, height: 30 }} />
+          )}
         </div>
         <ReactPlayer
           url="https://storage.googleapis.com/superpeer-prod.appspot.com/hosts/Wrg3QfdEPDmrjE8jQkLU/video-1591320054285-transcoded.mp4?fbclid=IwAR16D4raG-L50EW-If5O53Lvh3ASeAb_FNb02FuQLixtGosNFPvl9VSlMB0"
           playing
           loop
-          controls
+          volume={1}
+          muted={mute}
           width="100%"
           height="100%"
         />
